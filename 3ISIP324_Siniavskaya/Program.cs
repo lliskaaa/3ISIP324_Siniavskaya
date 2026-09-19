66c508583f0b6f58d4d13df78fb4a70cbb774913
lliskaa <alisasin90@gmail.com>
2026-09-19 09:20:02 +0300

    добавила перечень категорий товаров, класс товара и его свойства

--- 3ISIP324_Siniavskaya/Program.cs
+++ 3ISIP324_Siniavskaya/Program.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace _3ISIP324_Siniavskaya
 {
+    public enum Category
+    {
+        Vegetab = 1,
+        Fruits = 2,
+        Berri = 3
+
+    }
+    class Product
+    {
+        public int ID { get; }
+        public string Name { get; set; }
+        public double Prise { get; set; }
+        public int Quantity { get; set; }
+        public Category Category { get; set; }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+           
+
 
         }
     }
