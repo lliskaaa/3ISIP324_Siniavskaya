b7af82b1c844bfdb91efe978e9adec05b02fcede
lliskaaa <alisasin90@gmail.com>
2026-09-25 11:32:41 +0300

    создала метод добавления продуктов

--- 3ISIP324_Siniavskaya/Program.cs
+++ 3ISIP324_Siniavskaya/Program.cs
@@ -7,13 +7,14 @@ using System.Threading.Tasks;
 
 namespace _3ISIP324_Siniavskaya
 {
+
     public enum Category
     {
         Vegetab = 1,
         Fruits = 2,
         Berri = 3
-
     }
+
     class Product
     {
         public int ID { get; }
@@ -21,19 +22,18 @@ namespace _3ISIP324_Siniavskaya
         public double Price { get; set; }
         public int Quantity { get; set; }
         public Category Category { get; set; }
-
         private static int nextID = 1;
+
         public Product(string name, double price, int quantity, Category category)
         {
             ID = nextID;
             nextID++;
-
             Name = name;
             Price = price;
             Quantity = quantity;
             Category = category;
-        }
 
+        }
         public bool InStock
         {
             get
@@ -45,9 +45,9 @@ namespace _3ISIP324_Siniavskaya
         {
             Console.WriteLine("Код: " + ID);
             Console.WriteLine("Название: " + Name);
-            Console.WriteLine("Цена: " + Price + " руб.");
-            Console.WriteLine("Количество: " + Quantity);
-            Console.WriteLine("На складе: " + (InStock ? "Да" : "Нет"));
+            Console.WriteLine("Цена:  " + Price + "руб.");
+            Console.WriteLine("Количество:  " + Quantity);
+            Console.WriteLine("На складе:  " + (InStock ? "Да" : "Нет"));
             Console.WriteLine("Категория: " + Category);
         }
 
@@ -56,100 +56,49 @@ namespace _3ISIP324_Siniavskaya
     {
         static List<Product> products = new List<Product>();
 
-        static void Main(string[] args)
-        {
-            products.Add(new Product("Яблоки", 150, 10, Category.Fruits));
-            products.Add(new Product("Морковь", 80, 15, Category.Vegetab));
-            products.Add(new Product("Клубника", 300, 8, Category.Berri));
-            products.Add(new Product("Бананы", 120, 12, Category.Fruits));
-            products.Add(new Product("Помидоры", 200, 7, Category.Vegetab));
-
-            while (true)
-            {
-                Console.WriteLine();
-                Console.WriteLine("\nУЧЁТ ТОВАРОВ В МАГАЗИНЕ");
-                Console.WriteLine("1. Добавить товар");
-                Console.WriteLine("2. Удалить товар");
-                Console.WriteLine("3. Заказать поставку");
-                Console.WriteLine("4. Продать товар");
-                Console.WriteLine("5. Поиск товаров");
-                Console.WriteLine("0. Выход");
-
-                Console.Write("Выберите действие: ");
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        AddProduct();                 
-                        break;
-                    case "2":
-                        DeleteProduct();
-                        break;
-               
-                    case "0":
-                        return;
-                    default:
-                        Console.WriteLine("Неверная команда.");
-                        break;
-                }
-            }
-
-
-        }
         static void AddProduct()
         {
-            Console.Write("\nВведите название товара: ");
-            string name = Console.ReadLine();
-
-            if (name == "")
+            string name;
+            while (true)
             {
+                Console.Write("Введите название товара: ");
+                name = Console.ReadLine();
+                if (name != "")
+                {
+                    break;
+                }
                 Console.WriteLine("Название товара не может быть пустым.");
-                return;
             }
 
             double price;
-
             while (true)
             {
-                Console.Write("Введите цену: ");
+                Console.WriteLine("Введите цену: ");
                 string input = Console.ReadLine();
-
                 if (double.TryParse(input, out price) && price > 0)
                 {
                     break;
                 }
-
-                Console.WriteLine("Введите положительную цену.");
+                Console.WriteLine("Введите положительную цену!");
             }
 
             int quantity;
-
             while (true)
             {
-                Console.Write("Введите количество: ");
+                Console.WriteLine("Введите количество: ");
                 string input = Console.ReadLine();
-
                 if (int.TryParse(input, out quantity) && quantity >= 0)
                 {
                     break;
                 }
-
-                Console.WriteLine("Количество не может быть отрицательным.");
+                Console.WriteLine("Количество не может быть отрицательным!");
             }
-
             Category category;
-
             while (true)
             {
-                Console.WriteLine("Выберите категорию:");
-                Console.WriteLine("1. Овощи");
-                Console.WriteLine("2. Фрукты");
-                Console.WriteLine("3. Ягоды");
-
+                Console.WriteLine("Выберите категорию:\n 1-Овощи\n 2-Фрукты\n 3-Ягоды");
                 Console.Write("Ваш выбор: ");
                 string choice = Console.ReadLine();
-
                 switch (choice)
                 {
                     case "1":
@@ -165,53 +114,59 @@ namespace _3ISIP324_Siniavskaya
                         Console.WriteLine("Неверная категория!");
                         continue;
                 }
-
                 break;
             }
-
             Product product = new Product(name, price, quantity, category);
             products.Add(product);
-
-            Console.WriteLine("Товар добавлен.\n");
+            Console.WriteLine("Товар добавлен.");
             product.PrintInfo();
+
         }
-
-        static void DeleteProduct()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Список товаров:");
-
-            for (int i = 0; i < products.Count; i++)
-            {
-                Console.WriteLine(products[i].ID + "-" + products[i].Name);
-            }
-
-            int ID;
-
+            products.Add(new Product("Яблоки", 150, 10, Category.Fruits));
+            products.Add(new Product("Морковь", 80, 15, Category.Vegetab));
+            products.Add(new Product("Клубника", 300, 8, Category.Berri));
+            products.Add(new Product("Бананы", 120, 12, Category.Fruits));
+            products.Add(new Product("Помидоры", 200, 7, Category.Vegetab));
             while (true)
             {
-                Console.Write("Введите код товара, который хотите удалить: ");
-                string input = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("УЧЁТ ТОВАРОВ В МАГАЗИНЕ");
+                Console.WriteLine(" 1 — Добавить товар");
+                Console.WriteLine(" 2 — Удалить товар");
+                Console.WriteLine(" 3 — Заказать поставку");
+                Console.WriteLine(" 4 — Продать товар");
+                Console.WriteLine(" 5 — Поиск товаров");
+                Console.WriteLine(" 0 — Выход");
 
-                if (int.TryParse(input, out ID) && ID > 0)
+                Console.Write("Выберите действие: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
                 {
-                    break;
+                    case "1":
+                        AddProduct();
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        break;
+                    case "5":
+                        
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Неверная команда.");
+                        break;
                 }
-
-                Console.WriteLine("Введите правильный код!");
             }
-
-            for (int i = 0; i < products.Count; i++)
-            {
-                if (products[i].ID == ID)
-                {
-                    Console.WriteLine("Вы удаляете товар: " + products[i].Name);
-                    products.RemoveAt(i);
-                    Console.WriteLine("Товар удалён.");
-                    return;
-                }
-            }
-
-            Console.WriteLine("Товар с таким кодом не найден.");
         }
     }
 }
